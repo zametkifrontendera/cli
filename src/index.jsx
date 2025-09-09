2bf696202d5acefa31ee820c009a123de4dcc643
zametki.frontendera <zametki.frontendera@gmail.com>
2025-09-09 16:14:25 +0300

    fix bug for login validation

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import RegisterPage from "@/pages/RegisterPage/ui/RegisterPage";
+import { RegisterPage } from "@/pages/RegisterPage/ui/RegisterPage";
 import { LoginPage } from "@/pages/LoginPage/ui/LoginPage";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
