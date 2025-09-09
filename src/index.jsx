febb354bbc7b0fd972b143c8a0cff21ddd267ee3
zametki.frontendera <zametki.frontendera@gmail.com>
2025-09-09 14:48:27 +0300

    add render to register

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import RegisterPage from "@/pages/RegisterPage/ui/RegisterPage";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
@@ -8,6 +8,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
     <Routes>
+      <Route path="/" element={<Navigate to="/register" />} />
       <Route path="/register" element={<RegisterPage />} />
       {/* <Route path="/profile" element={<div>Страница профиля (позже)</div>} /> */}
     </Routes>
